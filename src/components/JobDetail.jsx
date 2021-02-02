8500c8e3c3688703cbd2d3eb0432c474384fde5b
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-02-02 17:36:45 +0100

    update

--- src/components/JobDetail.jsx
+++ src/components/JobDetail.jsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import React, { Component } from "react";
-import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 class JobDetail extends Component {
   htmlDesc = () => {
@@ -13,31 +13,32 @@ class JobDetail extends Component {
 
   render() {
     return this.props.job ? (
-      <div className="col-md-8 mt-5">
-        <div className="row mt-2">
-          <div className="col-sm-12">
-            <h1 className="mt-5">{this.props.job.title}</h1>
-            <h2 className="mt-2">{this.props.job.company}</h2>
+      <Container>
+        <div className="col-md-8 mt-5">
+          <div className="row mt-2">
+            <div className="col-sm-12">
+              <h1 className="mt-5">{this.props.job.title}</h1>
+              <h2 className="mt-2">{this.props.job.company}</h2>
+            </div>
           </div>
-        </div>
-        <div className="row mt-5">
-          <div className="col-sm-5 mt-3">
-            <img
-              className="job-cover"
-              src={this.props.job.company_logo}
-              alt="job selected"
-            />
-          </div>
-          <div className="col-sm-7">
-            <p>
-              <span className="font-weight-bold">Job Description:</span>{" "}
-              {this.htmlDesc()}
-            </p>
 
-            <a href={this.props.job.url} />
+          <div className="row mt-5">
+            <div className="col-sm-5 mt-3">
+              <img
+                className="job-cover"
+                src={this.props.job.company_logo}
+                alt="job selected"
+              />
+            </div>
+
+            <div className="col-sm-7">
+              <span className="font-weight-bold">Job Description:</span>{" "}
+              <div dangerouslySetInnerHTML={this.htmlDesc()}></div>
+              <a href={this.props.job.url} />
+            </div>
           </div>
         </div>
-      </div>
+      </Container>
     ) : (
       <div className="col-sm-8">
         <div className="row mt-2">
